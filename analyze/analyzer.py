a0e0d63b7153ed9a926e3956572129759e0193af
D. Hyuck Ahn <kickscar@gmail.com>
2017-10-13 15:53:03 +0900

    crawler 모듈 완성

--- analyze/analyzer.py
+++ analyze/analyzer.py
@@ -1,5 +1,8 @@
 import json
 import re
+from konlpy.tag import Twitter
+from collections import Counter
+
 
 def json_to_str(filename, key):
     jsonfile = open(filename, 'r', encoding='utf-8')
@@ -12,6 +15,14 @@ def json_to_str(filename, key):
         if value is None:
             continue
 
-        data = ' '.join((data, re.sub(r'[^\w]', '', value)))
+        data += re.sub(r'[^\w]', '', value)
 
     return data
+
+
+def count_wordfreq(data):
+    twitter = Twitter()
+    nouns = twitter.nouns(data)
+
+    count = Counter(nouns)
+    return count
