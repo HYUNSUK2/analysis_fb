9474bf408faeac76be3643216f7a7ea5fe0eb775
D. Hyuck Ahn <kickscar@gmail.com>
2017-10-13 10:47:46 +0900

    crawler 모듈 작성2

--- __test__/test_crawler.py
+++ __test__/test_crawler.py
@@ -1,11 +1,16 @@
 import analysis_fb.collection.crawler as crawler
 
+
 pagename = "jtbcnews"
 since = '2017-10-01'
 until = '2017-10-01'
 
-data = [('jtbcnews', '2017-10-01', '2017-10-05')]
+items = [
+    ('jtbcnews', '2017-01-01', '2017-10-05'),
+    ('chosun', '2017-10-01', '2017-10-05'),
 
-for pagename, since, until in data:
+]
+
+for pagename, since, until in items:
     crawler.crawling(pagename, since, until)
 
